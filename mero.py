8d72719334cc63d8cc56b85729ad5d1ceb82c4f4
deemero <neroalex93@gmail.com>
2021-08-04 21:54:32 +0800

    my refrence note for python

---
+++ mero.py
@@ -0,0 +1,72 @@
+# learn variable #
+# store = biscut
+
+# how to call the bicut
+# just code answer:
+# print(store) and the output will be = biscut
+
+
+# learn with string #
+# name = "mero"
+
+# print (name.replace("mero", "")) #dia ganti nama mero pergi  | jawapan =  ""
+# print (len(name))                    #dia kira berapa huruf dalam tue name  jawapan = 4
+# print (name.upper())                 #dia kasi huruf besar tue nama | jawapan = MERO
+# print (name.isupper())               #dia kasi tengok betul kha semua huruf besar dalam tue nama atau salah | jawapan = False
+# print (name.upper().isupper())       #dia tengok betul kha huruf besar tue nama didepan atau salah | jawapan = True
+# print (name[0])                      #dia kasi taw huruf apa start dari 0 | jawapan = m
+# print (name.index("m"))              #dia kasi taw "m" itu number apa | jawapan = 0
+
+
+# learn with number #
+# my_num = 5
+# from math import *
+
+# print(str(my_num) + " my feveret number") #str yang diblg tue string mau kasi convert to number pigi string sebab kalau tidak dia tidak dapat baca
+# print(my_num + "my feveret number")       #kalau ini dia tidak baca tue my number sebagai string sebab dia jadi number bukan sring kecuali tukar tue varible
+# my_num = "5"                              #tue number pigi string mcm disebalah <--
+# print(abs(my_num)) #kalau macam 5 dia abs tue dia kasi jadi lawan | jawapan = -5
+# print(pow(3, 2))  #dia kasi jadi kuasa mcm 3^2 bgitu | jawapan = 9
+# print(round(3.2))   #dia kasi genap contoh kalah 3.2 dia ambil 3 seja kalau 3.6 dia kasi genap pigi 4 | jawapan = 3
+# print(floor(3.2)) #kegunaan floor mesty ada from math import dimana dia akan ambil point di depan | jawapan = 3
+# prinr(sqrt(32)    # dia buat square root | jawpan = 6
+
+# print(5 + 5) #tambah
+# print(5 - 5) #tolak
+# print(5 * 5) #kali
+# print(5 % 5) #bahagi
+
+# get user input #
+
+# name = input("Enter Your Name: ")    #dia mcm gets.chomp jg tpi dia tukar p input sahaja
+# print("hello " + name + " Welcome")
+
+how to make a calculater #
+
+nums = input("ENTER A NUMBER: ")        #  cara buat caclculater tapi yang ini saya tambah int dalam varible "jawapan"
+num = input("ENTER ANOTHER NUMBER: ")   #1 sebab "int" dia akan tukar apa yang dalam user input pergi ke intiger
+jawapan = int(nums) + int(num)          #2 sebab kalau tidak letak "int" dia akan string bukan interger dalam input user
+
+print(jawapan)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
